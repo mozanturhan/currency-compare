9ccf06ea8f6d16e9a50237b632db93bda431abc5
Mehmet Ozan Turhan <m.ozanturhan@hotmail.com>
2019-04-11 00:56:57 +0300

    Logo

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('logo.svg', './assets/images/logo.svg')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -38,8 +39,6 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(false)
 
     // enables @babel/preset-env polyfills
     .configureBabel(() => {}, {
